862fa3950e3892e35f818070d750d2876c612bf4
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-08-09 14:30:15 -0300

    entradas e saídas de dados

---
+++ entradas_saidas.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*
+        Entrada e Saída
+        
+        count: saída
+        cin: saída
+    */
+
+    // imprimir Hello, World
+    cout << "Hello, World" << endl;
+
+}
