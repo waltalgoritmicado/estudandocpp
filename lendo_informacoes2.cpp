85104020d0bdf1663591ff890595432484ebbb2d
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-08-09 18:26:40 -0300

    lendo informacoes2

---
+++ lendo_informacoes2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int dia, mes, ano;
+
+    cout << "Entre com a data do seu aniversario (dd mm aa):";
+    cin >> dia >> mes >> ano;
+
+    cout << "Voce nasceu no dia " << dia << "/" << mes << "/" << ano <<     endl;
+
+}
+
