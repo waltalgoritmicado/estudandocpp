e6f279eeaa3e8ce6032f1a3671786d0172033905
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-08-09 19:13:05 -0300

    se num eh par?

---
+++ par_impar.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Entre com um valor inteiro: ";
+    cin >> num;
+    cout << "\nPar? " << (num % 2) << endl;
+}
