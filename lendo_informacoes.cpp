62b5c1b5a35e6942a82ff41c3091021393fae9e4
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-08-09 18:12:19 -0300

    informações do teclado

---
+++ lendo_informacoes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int idade;
+    cout << "entre a sua idade: ";
+    cin >> idade;
+
+    cout << "Voce tem " << idade << " anos." << endl;
+}
+
