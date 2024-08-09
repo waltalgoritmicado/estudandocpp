78e0eaa3b5ed4a17819a003057d0a3d1740eedad
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-08-09 14:01:23 -0300

    variaveis

---
+++ variaveis.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/*
+    int: inteiros
+    char: caracteres
+    float: reias
+*/
+
+int main(){
+    int pera;
+    char qualidade;
+    float peso;
+
+    pera = 3;
+    qualidade = 'A';
+    peso = 0.6453;
+}
